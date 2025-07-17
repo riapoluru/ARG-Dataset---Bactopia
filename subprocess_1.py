6d245c22723cd77252ed531caba23c48774e31c4
riapoluru <polururia@gmail.com>
2025-07-17 11:02:17 -0700

    subprocess

--- subprocess_1.py
+++ subprocess_1.py
@@ -1,1 +1,12 @@
 import subprocess
+import sys
+
+baap_command = ["bactopia", "--r1 /fastq/SRR15144755_1.fastq.gz", "--r2 /fastq/SRR15144755_1.fastq.gz", "--sample SRR15144755", "--outdir local-single-sample","-profile docker"]
+
+try:
+    print(" Running BaAP pipeline...\n")
+    subprocess.run(baap_command, check=True)
+    print("BaAP completed\n")
+except subprocess.CalledProcessError as e:
+    print(f"BaAP run failed: {e}\n")
+    sys.exit(1)
